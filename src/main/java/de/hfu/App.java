fa6d8967616be7c109ea353d0d4cc9aa0c7f88c9
Marian Stucke <stuckemarian@web.de>
2020-11-12 15:50:10 +0100

    added comments

--- src/main/java/de/hfu/App.java
+++ src/main/java/de/hfu/App.java
@@ -8,10 +8,21 @@ import java.util.Scanner;
  */
 public class App 
 {
+	
+	/**
+	 * In der main methode wird etwas von der tastatur eingelesen 
+	 * und in grossbuchstaben ausgegeben
+	 */
+	
     public static void main( String[] args )
     {
+    	//erstellen von scanner
         Scanner scanner = new Scanner(System.in);
+        //text einlesen
         String text = scanner.next();
+        //text ausgeben
         System.out.println(text.toUpperCase());
+        //scanner schliessen
+        scanner.close();
     }
 }
