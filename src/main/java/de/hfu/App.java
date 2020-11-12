8713ef66d245069846fcb8195262d46b4314b910
Marian Stucke <stuckemarian@web.de>
2020-11-12 15:25:37 +0100

    created project

---
+++ src/main/java/de/hfu/App.java
@@ -0,0 +1,17 @@
+package de.hfu;
+
+import java.util.Scanner;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Scanner scanner = new Scanner(System.in);
+        String text = scanner.next();
+        System.out.println(text.toUpperCase());
+    }
+}
