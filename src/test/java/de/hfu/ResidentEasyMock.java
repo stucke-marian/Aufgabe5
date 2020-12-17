37d2fcdf1c52e7847aa6322e0148613fa598cb57
Marian Stucke <stuckemarian@web.de>
2020-12-17 15:20:49 +0100

    added error

--- src/test/java/de/hfu/ResidentEasyMock.java
+++ src/test/java/de/hfu/ResidentEasyMock.java
@@ -38,35 +38,10 @@ public class ResidentEasyMock {
 		
 		verify(mockObject);
 		
-		assertThat(size1, equalTo(1));
+		//assertThat(size1, equalTo(1));
+		assertTrue(false);
 	}
 	
 	
-	@Test
-	public void test2() throws ResidentServiceException {
-		Resident resident1 = new Resident("Marian", "Stucke", "Strasse", "Furtwangen", new Date(2020,10,10));
-		Resident resident2 = new Resident("Peter", "Stucke", "Test", "Furtwangen", new Date(2020,10,10));
-		Resident resident3 = new Resident("Test", "Stucke", "Test", "Furtwangen", new java.util.Date(2020,10,10));
-		
-	    List<Resident> residentList1 = new ArrayList<>();
-		residentList1.add(resident1);
-		residentList1.add(resident2);
-		residentList1.add(resident3);
-		 
-		
-		ResidentRepository mockObject = createMock(ResidentRepository.class);
-		expect(mockObject.getResidents()).andReturn(residentList1);
-		
-		replay(mockObject);
-		
-		BaseResidentService residentService = new BaseResidentService();
-		residentService.setResidentRepository(mockObject);
-		
-		Resident resident = residentService.getUniqueResident(resident3);
-		
-		
-		verify(mockObject);
-		
-		assertThat(resident, equalTo(resident3));
-	}
+
 }
